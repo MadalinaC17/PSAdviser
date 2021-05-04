5c55f5e55733ed681c2cc5c6bc49c513c4360de2
Madalina <ivum@yahoo.com>
2021-05-04 16:23:31 +0100

    FormsExample

--- src/Components/Pages/SecondMainPage.js
+++ src/Components/Pages/SecondMainPage.js
@@ -1,9 +1,9 @@
 
 import React from 'react';
-
 import '../DesignCSS/SecondMainPage.css';
 import logo from "../../Imagesps/Psadviserlogo.png";
-
+import { Link } from 'react-router-dom';
+import '../../App.css';
 
 export default function SecondMainPage() {
   return (
@@ -13,8 +13,10 @@ export default function SecondMainPage() {
           </div>
           <div class="align">
 <div class="btn-group-vertical">
+<Link to ='/clientdetails'>
 
 <button className="button btn-outline-secondary btn-block">Client details & Risk Assesment</button>
+</Link>
 <button className="button btn-outline-secondary btn-block">Police Station Section         </button>
 <button className="button btn-outline-secondary btn-block">Briefing Notes                 </button>
 <button className="button btn-outline-secondary btn-block ">Police Codes of Conduct       </button>
