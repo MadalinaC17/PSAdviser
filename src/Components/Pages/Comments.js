838e777e74277f2b6ecfc14e9d0e047e25b9cd16
Madalina <ivum@yahoo.com>
2021-05-04 15:41:09 +0100

    Comments page done

--- src/Components/Pages/Comments.js
+++ src/Components/Pages/Comments.js
@@ -1,7 +1,35 @@
 
 import React from 'react';
-import '../../App.css';
+//import '../../App.css';
+import '../DesignCSS/Comments.css';
+import {Form,Button} from 'react-bootstrap';
+
 
 export default function Comments() {
-  return <h1 className='comments'>Comments</h1>;
-}
+  return (
+<div className ="comments">
+<div className = "font">
+<Form>
+
+
+<Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Label className = "text">Adviser notes</Form.Label>
+    <Form.Control className= "label" as="textarea" rows={5} />
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Label className = "text" >Client notes</Form.Label>
+    <Form.Control className= "label" as="textarea" rows={5} />
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Label className = "text">Comments</Form.Label>
+    <Form.Control className= "label" as="textarea" rows={5} />
+  </Form.Group>
+  
+  <Button  className=' btnsubmit btn-outline-secondary '  type="submit">
+    Submit
+  </Button>
+</Form>
+</div>
+</div>
+  );
+};
